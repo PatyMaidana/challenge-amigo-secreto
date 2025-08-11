bf238d82d8de6c9fbdbd4d3d3b50c285617243f3
Patricia Maidana <patricia.rmaidana@gmail.com>
2025-08-11 08:04:12 -0300

    add função adicionar amigos com verificador de campo vazio

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let amigos = []
+
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo").value
+    if (nomeAmigo === "") {
+        alert("Por favor, insira o nome do amigo.")
+        return
+    }
+    amigos.push(nomeAmigo)
+    document.getElementById("amigo").value = ""
+    console.log(amigos)
+    listarAmigos()
+}
