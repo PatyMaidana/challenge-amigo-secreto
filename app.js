973d95911323a74a56bd2dcae08d9c5640de50a5
Patricia Maidana <patricia.rmaidana@gmail.com>
2025-08-17 10:39:08 -0300

    adicionar função sortearAmigos e função Exibir resultados

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ let amigos = []
 
 function adicionarAmigo() {
     let nomeAmigo = document.getElementById("amigo").value
-    if (nomeAmigo === "") {
+        if (nomeAmigo === "") {
         alert("Por favor, insira o nome do amigo-secreto.")
+        return
+    }
+    if (amigos.includes(nomeAmigo)) {
+        alert("Este amigo já foi adicionado.")
         return
     }
     amigos.push(nomeAmigo)
@@ -20,3 +24,39 @@ function listarAmigos() {
     lista += "</ul>"
     document.getElementById("listaAmigos").innerHTML = lista
 }
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Adicione pelo menos dois amigos para sortear.")
+        return
+    }
+
+    let sorteios = {}
+    let amigosDisponiveis = [...amigos]
+
+    for (let i = 0; i < amigos.length; i++) {
+        let amigoAtual = amigos[i]
+        let indexSorteado = Math.floor(Math.random() * amigosDisponiveis.length)
+        let amigoSorteado = amigosDisponiveis[indexSorteado]
+
+        // Evita que alguém tire a si mesmo
+        while (amigoSorteado === amigoAtual) {
+            indexSorteado = Math.floor(Math.random() * amigosDisponiveis.length)
+            amigoSorteado = amigosDisponiveis[indexSorteado]
+        }
+
+        sorteios[amigoAtual] = amigoSorteado
+        amigosDisponiveis.splice(indexSorteado, 1)
+    }   
+     exibirResultados(sorteios)
+} 
+function exibirResultados(sorteios) {
+    let resultados = "<h2>Resultados do Sorteio:</h2><ul>"
+    for (let amigo in sorteios) {
+        resultados += "<li>" + amigo + " tirou " + sorteios[amigo] + "</li>"
+    }
+    resultados += "</ul>"
+    document.getElementById("resultado").innerHTML = resultados
+}
+
+
+
